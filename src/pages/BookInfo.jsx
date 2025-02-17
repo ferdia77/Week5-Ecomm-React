466f863bbc2415ac5445cb3da61c6695f2a5adab
David <kingqueensuited22@gmai.com>
2025-02-17 11:41:20 +0100

    error

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Rating from "../components/ui/Rating";
+import Price from "../components/ui/Price";
 
 const BookInfo = ({ books }) => {
     return (
@@ -11,9 +13,21 @@ const BookInfo = ({ books }) => {
                         <Link to="/books" className="book__link">
                             <FontAwesomeIcon icon="arrow-left" />
                         </Link>
-                        <Link to="/book">
+                        <Link to="/books" className="book__link">
                             <h2 className="book__selected--title--top">Books</h2>
                         </Link>
+                    </div>
+                    <div className="book__selected">
+                        <figure className="book__selected--figure">
+                            <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__seleced--img" />
+                        </figure>
+                        <div className="book__selected--description">
+                            <h2 className="book__selected--title">Crackthe coding interview</h2>
+                            <Rating rating="4.5" />
+                            <div className="book__selected--price">
+                            <Price originalPrice={50} salePrice={20} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
