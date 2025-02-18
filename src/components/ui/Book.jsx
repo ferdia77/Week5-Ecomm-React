d550e7efb79742b8656df2f37a021f400113394a
David <kingqueensuited22@gmai.com>
2025-02-18 19:13:02 +0100

    issue with 'find'

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -5,18 +5,15 @@ import Rating from "./Rating";
 import Price from "./Price";
 
 const Book = ({ book }) => {
-    const rating = 4;
-
-
   return (
     <div className="book">
-      <Link to="/books/1">
+      <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img src={book.url} alt="" />
         </figure>
       </Link>
       <div className="book__title">
-        <Link to="/books/1" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
--- src/components/ui/Price.jsx
+++ src/components/ui/Price.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Price = (salePrice, originalPrice) => {
+const Price = ({salePrice, originalPrice}) => {
     return (
         <div>
            <div className="book__price">
         {salePrice ? (
           <>
-            <span className="book__price--normal">${originalPrice.toFixed(2)}</span>$
+          <span className="book__price--normal">${originalPrice.toFixed(2)}</span>$
             {salePrice.toFixed(2)}
           </>
         ) : (
